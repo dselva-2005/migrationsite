6dee2e1be62563afd956803f5d59d84fc1599165
dselva-2005 <dselvajagan@gmail.com>
2026-01-08 06:28:49 +0530

    production ready with dummy data and config

--- backend/CoreApi/settings/prod.py
+++ backend/CoreApi/settings/prod.py
@@ -5,24 +5,24 @@ DEBUG = False
 
 DOMAIN = os.environ.get("DOMAIN")
 
-ALLOWED_HOSTS = [DOMAIN]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 CSRF_TRUSTED_ORIGINS += [
     f"https://{DOMAIN}",
+    f"https://www.{DOMAIN}",
 ]
 
 CORS_ALLOWED_ORIGINS += [
     f"https://{DOMAIN}",
+    f"https://www.{DOMAIN}",
 ]
 
-# --------------------------------------------------
-# HTTPS & Security
-# --------------------------------------------------
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+USE_X_FORWARDED_HOST = True
+
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 REST_FRAMEWORK.update({"DEFAULT_RENDERER_CLASSES": (
         "rest_framework.renderers.JSONRenderer",
